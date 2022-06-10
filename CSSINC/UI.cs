2181b77244a74069080f46f7797bc31fa299edfb
Chris Gauthier <chris@elektrus.ca>
2022-06-10 12:35:20 -0500

    First Commit

---
+++ CSSINC/UI.cs
@@ -0,0 +1,38 @@
+namespace CSSINC;
+
+public partial class UI : Form
+{
+    public UI()
+    {
+        InitializeComponent();
+        
+    }
+
+    private void UI_Load(object sender, EventArgs e)
+    {
+        String text = "hello world";
+
+        TranslationService.TextTranslation.translateText(text);
+        
+    }
+
+    private void textBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void label1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Translate_Click(object sender, EventArgs e)
+    {
+        String text1;
+        text1 = TranslationService.TextTranslation.translateText(inputString.Text);
+        outputString.Text = text1;
+
+
+        
+    }
+}
---
+++ TranslationService/TextTranslation.cs
@@ -0,0 +1,15 @@
+﻿namespace TranslationService;
+using Google.Cloud.Translation.V2;
+public class TextTranslation
+{
+    public static String translateText(String textToBeTranslated)
+    {
+
+        var client = TranslationClient.Create();
+        var response = client.TranslateText(textToBeTranslated, LanguageCodes.French, LanguageCodes.English);
+        return response.TranslatedText;
+        
+    }
+
+
+}
